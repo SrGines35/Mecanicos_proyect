b1881b56432eebf441952161a354f6e9256249ba
luuzj <luzjatziri5@gmail.com>
2026-08-20 02:35:03 -0600

    refactor: actualizar estructura del modulo de usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,36 +11,6 @@ export const routes: Routes = [
     loadComponent: () => import('./features/registro/registro').then((m) => m.Registro),
     title: 'Oaxicanicos - Crear cuenta',
   },
-
-  {
-    path: 'usuario',
-    loadComponent: () =>
-      import('./features/usuario/layout/layout').then((m) => m.Layout),
-    children: [
-      {
-        path: '',
-        redirectTo: 'inicio',
-        pathMatch: 'full',
-      },
-      {
-        path: 'inicio',
-        loadComponent: () => import('./features/usuario/inicio/inicio').then((m) => m.Inicio),
-      },
-      {
-        path: 'perfil',
-        loadComponent: () => import('./features/usuario/perfil/perfil').then((m) => m.Perfil),
-      },
-      {
-        path: 'vehiculos',loadComponent: () => import('./features/usuario/vehiculos/vehiculos').then((m) => m.Vehiculos),
-      },
-      {
-        path: 'configuracion',loadComponent: () => import('./features/usuario/configuracion/configuracion').then((m) => m.Configuracion),
-      },
-      {
-        path: 'solicitar-servicio',loadComponent: () => import('./features/usuario/solicitar-servicio/solicitar-servicio').then((m) => m.SolicitarServicio),
-      },
-    ],
-  },
   { path: '**', redirectTo: '' },
 
 
