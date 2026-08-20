db05f0af1006879e86b36c7df4844c9e517297e1
luuzj <luzjatziri5@gmail.com>
2026-08-20 02:52:59 -0600

    feat: crear rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,36 @@ export const routes: Routes = [
     loadComponent: () => import('./features/registro/registro').then((m) => m.Registro),
     title: 'Oaxicanicos - Crear cuenta',
   },
+  {
+    path: 'usuario',
+    loadComponent: () =>
+      import('./features/usuario/layout/layout')
+        .then(m => m.Layout),
+
+    children: [
+
+      { path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+
+      {
+        path: 'home',
+        loadComponent: () => import('./features/usuario/home/home').then(m => m.Home)
+      },
+
+      {
+        path: 'perfil',
+        loadComponent: () =>import('./features/usuario/perfil/perfil').then(m => m.Perfil)
+      },
+
+      {
+        path: 'solicitar-servicio',
+        loadComponent: () =>import('./features/usuario/solicitar-servicio/solicitar-servicio').then(m => m.SolicitarServicio)
+      }
+
+    ]
+  },
   { path: '**', redirectTo: '' },
 
 
