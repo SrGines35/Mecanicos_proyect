ef78ed1e9482dd397bcb6297d63534e28e91b574
luuzj <luzjatziri5@gmail.com>
2026-08-19 22:54:53 -0600

    feat: crear navbar responsive

--- src/app/features/usuario/layout/layout.ts
+++ src/app/features/usuario/layout/layout.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './layout.html',
-  styleUrl: './layout.css',
+  styleUrl: './layout.css'
 })
 export class Layout {
+  isMenuOpen = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
 }
