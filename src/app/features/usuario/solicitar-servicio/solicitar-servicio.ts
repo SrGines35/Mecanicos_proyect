6a0964a92ac699cb4aa69557ae2b5fab5c7bb1aa
luuzj <luzjatziri5@gmail.com>
2026-08-20 02:50:12 -0600

    feat: crear estructura del modulo de usuario

---
+++ src/app/features/usuario/solicitar-servicio/solicitar-servicio.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitar-servicio',
+  imports: [],
+  templateUrl: './solicitar-servicio.html',
+  styleUrl: './solicitar-servicio.css',
+})
+export class SolicitarServicio {
+
+}
